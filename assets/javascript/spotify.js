2730c6fafc6339fc9d19b8b4a31dc9e2a514af84
mikek4 <mike.katz@outlook.com>
2018-12-22 11:52:48 -0500

    Added youtube API (#6)

---
+++ assets/javascript/spotify.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('#search-term').submit(function (event) {
+        event.preventDefault();
+        var searchTerm = $('#query').val();
+        getRequest(searchTerm);
+    });
+});
+
+function getRequest(searchTerm) {
+    var url = 'https://www.googleapis.com/youtube/v3/search';
+    var params = {
+        part: 'snippet',
+        key: 'AIzaSyBoeAORrZ6eh2-ayyufG7OVOu24CKb_Ww8',
+        q: searchTerm
+    };
+  
+    $.getJSON(url, params, showResults);
+}
+
+function showResults(results) {
+    var html = "";
+    var entries = results.items;
+    
+    $.each(entries, function (index, value) {
+        var title = value.snippet.title;
+        var thumbnail = value.snippet.thumbnails.default.url;
+        html += '<p>' + title + '</p>';
+        html += '<img src="' + thumbnail + '">';
+    }); 
+    
+    console.log(results)
+    $('#soundtrack-info').html(html);
+}
+
