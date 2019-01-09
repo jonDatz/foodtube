63ca11a130fb923668717d45d400eb0fedb33ea3
mikek4 <michaelkatz@Michaels-MacBook-Pro.local>
2019-01-08 21:11:34 -0500

    Added ingredients

--- assets/javascript/edamam.js
+++ assets/javascript/edamam.js
@@ -3,18 +3,20 @@ $(document).ready(function () {
         event.preventDefault();
         var searchTerm = $('#query').val();
         getRequest(searchTerm);
-        console.log(searchTerm);
 
     var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=8c4edf85&app_key=658de4f65177123c991d45cf851c94cd";
-
-    console.log(queryURL);
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-
+        console.log(response.hits[0].recipe.ingredients)
+        var ingredients = response.hits[0].recipe.ingredients;
+        for (var i=0; i < ingredients.length; i++){
+            $("#movie-info").append(ingredients[i].text);
+        }
+        
     });
 
 
